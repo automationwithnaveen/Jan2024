efb65184c5a933318fbfaf1b17f6e2996fcbc555
Naveen <Naveen@Naveens-MacBook-Pro-2.local>
2024-06-30 09:44:37 +0100

    First Commit

---
+++ src/Demo2.java
@@ -0,0 +1,11 @@
+
+public class Demo2 {
+
+	public static void main(String n[])
+	{
+		System.out.println("Naveen Kumar");
+	}
+
+}
+
+
---
+++ src/Demo3.java
@@ -0,0 +1,24 @@
+
+
+// float bill = 250.50f;
+// int discountAmount = 50;
+// bill = bill + discountAmount
+//  i) Identify number of variables in above Program?
+//  ii) Identify the Variable names in above program?
+//  iii) At end of Program what is the value of bill?
+
+
+
+public class Demo3 
+{
+	public static void main(String n[])
+	{
+		
+		System.out.println("Hello This is Naveen");
+		
+		System.out.print("I Live in MK");
+		
+		System.out.println("UK");
+		
+	}
+}
